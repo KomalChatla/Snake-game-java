1a3c2179778a255aac7ce5c70d113ff653811349
KomalChatla <91951453+KomalChatla@users.noreply.github.com>
2024-02-26 16:46:47 +0530

    Add files via upload

---
+++ src/Food.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+public class Food {
+
+    private final int posX;
+    private final int posY;
+
+    public Food() {
+        posX = generatePos(Snake.WIDTH);
+        posY = generatePos(Snake.HEIGHT);
+    }
+
+    private int generatePos(int size) {
+        Random random = new Random();
+        return random.nextInt(size / Snake.TICK_SIZE) * Snake.TICK_SIZE;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+}
